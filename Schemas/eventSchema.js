d870561d81a3475143bbfec463137b3f32606f92
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2025-02-07 12:20:36 +0600

    add event schema for event management; define fields for name, description, date, time, location, organizer, image, and attendees

---
+++ Schemas/eventSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const eventSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    organizer:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    attendees:{
+        type:Array,
+        required:true
+    }
+
+})
+
+module.exports = eventSchema;
